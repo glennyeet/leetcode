c9f358210813d8ef4d812aaf6c913fd5bd318d51
Glenn Ye <lcm7063@gmail.com>
2024-09-16 22:35:59 -0400

    Update 539

--- 539. Minimum Time Difference/findMinDifference.py
+++ 539. Minimum Time Difference/findMinDifference.py
@@ -1,13 +1,40 @@
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
-        # def compare_times(time1: str, time2: str) -> int:
-        #     if time1[:2] > time2[:2]:
-        #         return 1
-        #     if time1[3:] > time2[3:]:
-        #         return 1
-        #     if time1 == time2:
-        #         return 0
-        #     return -1
+        def compare_times(time1: str, time2: str) -> int:
+            if time1 == time2:
+                return 0
+            if (
+                time1[:2] > time2[:2]
+                or time1[:2] == time2[:2]
+                and time1[3:] > time2[3:]
+            ):
+                return 1
+            return -1
+
+        def merge_sort(times: list[str]):
+            if len(times) > 1:
+                mid = len(times) // 2
+                left = times[:mid]
+                right = times[mid:]
+                merge_sort(left)
+                merge_sort(right)
+                i = j = k = 0
+                while i < len(left) and j < len(right):
+                    if compare_times(left[i], right[j]) == -1:
+                        times[k] = left[i]
+                        i += 1
+                    else:
+                        times[k] = right[j]
+                        j += 1
+                    k += 1
+                while i < len(left):
+                    times[k] = left[i]
+                    i += 1
+                    k += 1
+                while j < len(right):
+                    times[k] = right[j]
+                    j += 1
+                    k += 1
 
         def find_minutes_diff(time1: str, time2: str) -> int:
             # time1 <= time2
@@ -17,16 +44,16 @@ class Solution:
             hours2 = int(time2[:2])
             minutes1 = int(time1[3:])
             minutes2 = int(time2[3:])
-            hours = hours2 - hours1
             if minutes1 > minutes2:
-                hours -= 1
-            if minutes1 > minutes2:
+                hours = hours2 - hours1 - 1
                 minutes = 60 - minutes1 + minutes2
             else:
+                hours = hours2 - hours1
                 minutes = minutes2 - minutes1
             return hours * 60 + minutes
 
-        timePoints.sort()
+        # timePoints.sort()
+        merge_sort(timePoints)
         min_diff = (
             find_minutes_diff(timePoints[-1], "23:59")
             + find_minutes_diff("00:00", timePoints[0])
