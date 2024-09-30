0f1f0adcd88caa9eed9e48e3366682ad206ebe5a
Glenn Ye <lcm7063@gmail.com>
2024-09-29 23:58:00 -0400

    Update 1381

--- 1381. Design a Stack With Increment Operation/CustomStack.py
+++ 1381. Design a Stack With Increment Operation/CustomStack.py
@@ -1,30 +1,61 @@
+# O(k) increment
+# class CustomStack:
+
+#     def __init__(self, maxSize: int):
+#         self.stack = []
+#         self.max_size = maxSize
+
+#     def push(self, x: int) -> None:
+#         if len(self.stack) != self.max_size:
+#             self.stack.append(x)
+
+#     def pop(self) -> int:
+#         if not len(self.stack):
+#             return -1
+#         return self.stack.pop()
+
+#     def increment(self, k: int, val: int) -> None:
+#         if k < len(self.stack):
+#             unchanged_elements = []
+#             for _ in range(len(self.stack) - k):
+#                 unchanged_elements.append(self.stack.pop())
+#             for i in range(len(self.stack)):
+#                 self.stack[i] += val
+#             for _ in range(len(unchanged_elements)):
+#                 self.stack.append(unchanged_elements.pop())
+#         else:
+#             for i in range(len(self.stack)):
+#                 self.stack[i] += val
+
+
+# O(1) increment
 class CustomStack:
 
     def __init__(self, maxSize: int):
-        self.stack = []
+        self.stack = [0] * maxSize
+        self.increments = [0] * maxSize
+        self.top = -1
         self.max_size = maxSize
 
     def push(self, x: int) -> None:
-        if len(self.stack) != self.max_size:
-            self.stack.append(x)
+        if self.top < self.max_size - 1:
+            self.top += 1
+            self.stack[self.top] = x
 
     def pop(self) -> int:
-        if not len(self.stack):
+        if self.top == -1:
             return -1
-        return self.stack.pop()
+        value = self.stack[self.top] + self.increments[self.top]
+        if self.top > 0:
+            self.increments[self.top - 1] += self.increments[self.top]
+        self.increments[self.top] = 0
+        self.top -= 1
+        return value
 
     def increment(self, k: int, val: int) -> None:
-        if k < len(self.stack):
-            unchanged_elements = []
-            for _ in range(len(self.stack) - k):
-                unchanged_elements.append(self.stack.pop())
-            for i in range(len(self.stack)):
-                self.stack[i] += val
-            for _ in range(len(unchanged_elements)):
-                self.stack.append(unchanged_elements.pop())
-        else:
-            for i in range(len(self.stack)):
-                self.stack[i] += val
+        if self.top >= 0:
+            k_index = min(k - 1, self.top)
+            self.increments[k_index] += val
 
 
 # Your CustomStack object will be instantiated and called as such:
