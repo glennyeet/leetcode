5d41dca6a0152698b294cb979654ee514fe695ad
Glenn Ye <lcm7063@gmail.com>
2024-10-19 02:47:21 -0400

    Update 1545

--- 1545. Find Kth Bit in Nth Binary String/findKthBit.py
+++ 1545. Find Kth Bit in Nth Binary String/findKthBit.py
@@ -1,15 +1,36 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        def reverse_invert_string(string: str) -> str:
-            new_string = ""
-            for c in string:
-                if c == "0":
-                    new_string += "1"
-                else:
-                    new_string += "0"
-            return new_string[::-1]
+        # Brute Force
+        # def reverse_invert_string(string: str) -> str:
+        #     new_string = ""
+        #     for c in string:
+        #         if c == "0":
+        #             new_string += "1"
+        #         else:
+        #             new_string += "0"
+        #     return new_string[::-1]
 
-        binary_string = "0"
-        for _ in range(n - 1):
-            binary_string = binary_string + "1" + reverse_invert_string(binary_string)
-        return binary_string[k - 1]
+        # binary_string = "0"
+        # for _ in range(n - 1):
+        #     binary_string = binary_string + "1" + reverse_invert_string(binary_string)
+        # return binary_string[k - 1]
+
+        # Recursion
+        length = 2**n - 1
+
+        def get_bit(length: int, k: int) -> str:
+            if length == 1:
+                return "0"
+            mid = length // 2
+            if k == mid + 1:
+                return "1"
+            if k <= mid:
+                return get_bit(mid, k)
+            elif k > mid + 1:
+                bit = get_bit(mid, length + 1 - k)
+                if bit == "0":
+                    return "1"
+                else:
+                    return "0"
+
+        return get_bit(length, k)
