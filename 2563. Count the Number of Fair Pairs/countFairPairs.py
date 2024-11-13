33d5eb5f20d253b761d8a65c1334ae36fbbaf67c
Glenn Ye <lcm7063@gmail.com>
2024-11-13 01:03:59 -0500

    Complete 2563

---
+++ 2563. Count the Number of Fair Pairs/countFairPairs.py
@@ -0,0 +1,15 @@
+from bisect import bisect_left, bisect_right
+
+
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        sorted_nums = nums.copy()
+        sorted_nums.sort()
+        fair_pairs = 0
+        for i, num in enumerate(sorted_nums):
+            if num > abs(upper):
+                break
+            fair_pairs += bisect_right(sorted_nums, upper - num, i + 1) - bisect_left(
+                sorted_nums, lower - num, i + 1
+            )
+        return fair_pairs
