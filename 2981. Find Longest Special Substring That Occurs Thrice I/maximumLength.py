99a3198a380a905b25f1125ae2664fe6c1a9d2ed
Glenn Ye <lcm7063@gmail.com>
2024-12-10 13:48:48 -0500

    Complete 2981

---
+++ 2981. Find Longest Special Substring That Occurs Thrice I/maximumLength.py
@@ -0,0 +1,28 @@
+from collections import defaultdict, Counter
+
+
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        streak_counter = defaultdict(Counter)
+        current_char = None
+        streak = 0
+
+        def store_streaks():
+            for streak_len in range(1, streak + 1):
+                streak_counter[current_char][streak_len] += streak + 1 - streak_len
+
+        for char in s:
+            if char != current_char:
+                store_streaks()
+                current_char = char
+                streak = 1
+            else:
+                streak += 1
+        else:
+            store_streaks()
+        max_len = -1
+        for char in streak_counter:
+            for streak_len in streak_counter[char]:
+                if streak_counter[char][streak_len] >= 3:
+                    max_len = max(max_len, streak_len)
+        return max_len
