bbe2b3956de80b717375909c04d9c072c676c514
Glenn Ye <lcm7063@gmail.com>
2024-12-29 17:37:26 -0500

    Update 1639

--- 1639. Number of Ways to Form a Target String Given a Dictionary/numWays.py
+++ 1639. Number of Ways to Form a Target String Given a Dictionary/numWays.py
@@ -3,24 +3,26 @@ from functools import cache
 
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
-        N = len(words)
-        W = len(words[0])
-        T = len(target)
+        # Top-down DP: O(nt) time, O(nt) space
+
+        n = len(words)
+        w = len(words[0])
+        t = len(target)
         mod_factor = 10**9 + 7
 
         @cache
         def count_char_ways(word_index: int, char: str) -> int:
             ways = 0
-            for i in range(N):
+            for i in range(n):
                 if words[i][word_index] == char:
                     ways += 1
             return ways
 
         @cache
         def count_ways(word_index: int, target_index: int) -> int:
-            if target_index == T:
+            if target_index == t:
                 return 1
-            elif word_index == W:
+            elif word_index == w:
                 return 0
             ways = 0
             ways += count_char_ways(word_index, target[target_index]) * count_ways(
