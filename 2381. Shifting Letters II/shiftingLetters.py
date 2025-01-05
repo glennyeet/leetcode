1e07a1bb6417ec8d9d6cd6014fccc65407336c9f
Glenn Ye <lcm7063@gmail.com>
2025-01-05 01:34:59 -0500

    Update 2381

--- 2381. Shifting Letters II/shiftingLetters.py
+++ 2381. Shifting Letters II/shiftingLetters.py
@@ -6,16 +6,16 @@ class Solution:
         prefix_diffs = [0] * (n + 1)
         for start, end, direction in shifts:
             if direction == 1:
-                prefix_diffs[start] += -1
-                prefix_diffs[end + 1] += 1
-            else:
                 prefix_diffs[start] += 1
                 prefix_diffs[end + 1] += -1
+            else:
+                prefix_diffs[start] += -1
+                prefix_diffs[end + 1] += 1
         shift = 0
         s_list = []
         for char in s:
             s_list.append(ord(char) - ord("a"))
-        for i in reversed(range(n)):
-            shift += prefix_diffs[i + 1]
-            s_list[i] = chr((s_list[i] + shift) % 26 + ord("a"))
+        for i, num in enumerate(s_list):
+            shift += prefix_diffs[i]
+            s_list[i] = chr((num + shift) % 26 + ord("a"))
         return "".join(s_list)
