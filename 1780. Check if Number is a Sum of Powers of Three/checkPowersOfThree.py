fec1b1e420792bd97dc45c82d649f6869bdccfcc
Glenn Ye <lcm7063@gmail.com>
2025-03-03 19:56:56 -0500

    Complete 1780

---
+++ 1780. Check if Number is a Sum of Powers of Three/checkPowersOfThree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        # Backtracking: O(n) time, O(n) space
+
+        def is_powers_of_three_sum(cur_sum: int, exponent: int) -> bool:
+            cur_power = 3**exponent
+            if cur_sum == n:
+                return True
+            elif cur_sum > n or cur_power > n:
+                return False
+            if is_powers_of_three_sum(cur_sum + cur_power, exponent + 1):
+                return True
+            return is_powers_of_three_sum(cur_sum, exponent + 1)
+
+        return is_powers_of_three_sum(0, 0)
