0fe80e5f5ffc296e0b1414b8459646785d1bae5f
Glenn Ye <lcm7063@gmail.com>
2025-05-24 22:09:38 -0400

    Complete 2131

---
+++ 2131. Longest Palindrome by Concatenating Two Letter Words/longestPalindrome.py
@@ -0,0 +1,20 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        words_counter = Counter(words)
+        longest_palindrome = 0
+        palindrome_odd_occurence = False
+        for word in words_counter:
+            reversed_word = word[::-1]
+            if word == reversed_word:
+                if words_counter[word] % 2:
+                    palindrome_odd_occurence = True
+                longest_palindrome += words_counter[word] // 2 * 2 * 2
+            else:
+                longest_palindrome += (
+                    min(words_counter[word], words_counter[reversed_word]) * 2
+                )
+        return longest_palindrome + int(palindrome_odd_occurence) * 2
