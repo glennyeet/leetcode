a7feba660daa2e8a8de3917f3e333021b0a0fbbc
Glenn Ye <lcm7063@gmail.com>
2025-08-09 18:13:54 -0400

    Update 2106

--- 2106. Maximum Fruits Harvested After at Most K Steps/maxTotalFruits.py
+++ 2106. Maximum Fruits Harvested After at Most K Steps/maxTotalFruits.py
@@ -14,18 +14,18 @@ class Solution:
         l = min_position
         for r in range(min_position, max_position):
             cur_fruits += fruits[r][1]
-            left_steps = fruits[l][0]
-            right_steps = fruits[r][0]
+            leftmost_position = fruits[l][0]
+            rightmost_position = fruits[r][0]
             while (
                 l <= r
                 and min(
-                    2 * right_steps - startPos - left_steps,
-                    startPos - 2 * left_steps + right_steps,
+                    startPos - 2 * leftmost_position + rightmost_position,
+                    2 * rightmost_position - startPos - leftmost_position,
                 )
                 > k
             ):
                 cur_fruits -= fruits[l][1]
                 l += 1
-                left_steps = fruits[l][0]
+                leftmost_position = fruits[l][0]
             max_fruits = max(max_fruits, cur_fruits)
         return max_fruits
