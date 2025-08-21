936624403f9c0de7327d3c522c66cd3e5b48e4c1
Glenn Ye <lcm7063@gmail.com>
2025-08-21 02:18:17 -0400

    Update 1277

--- 1277. Count Square Submatrices with All Ones/countSquares2.py
+++ 1277. Count Square Submatrices with All Ones/countSquares2.py
@@ -7,17 +7,17 @@ class Solution:
 
         m = len(matrix)
         n = len(matrix[0])
-        valid_submatricies = 0
-        max_sides = [[0] * n for _ in range(m)]
+        total = 0
+        valid_submatricies = [[0] * n for _ in range(m)]
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 1:
-                    max_sides[i][j] = 1
+                    valid_submatricies[i][j] = 1
                     if i - 1 >= 0 and j - 1 >= 0:
-                        max_sides[i][j] += min(
-                            max_sides[i - 1][j],
-                            max_sides[i - 1][j - 1],
-                            max_sides[i][j - 1],
+                        valid_submatricies[i][j] += min(
+                            valid_submatricies[i - 1][j],
+                            valid_submatricies[i - 1][j - 1],
+                            valid_submatricies[i][j - 1],
                         )
-                valid_submatricies += max_sides[i][j]
-        return valid_submatricies
+                total += valid_submatricies[i][j]
+        return total
