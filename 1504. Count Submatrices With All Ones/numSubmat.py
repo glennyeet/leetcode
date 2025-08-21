c11dd10a00ec002c0bef7abce8140858b0f0f812
Glenn Ye <lcm7063@gmail.com>
2025-08-21 02:17:57 -0400

    Complete 1504

---
+++ 1504. Count Submatrices With All Ones/numSubmat.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def numSubmat(self, mat: List[List[int]]) -> int:
+        # Bottom-up DP: O(m^2 * n) time, O(mn) space
+
+        m = len(mat)
+        n = len(mat[0])
+        prefix_sums = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i == 0:
+                    prefix_sums[i][j] = mat[i][j]
+                else:
+                    prefix_sums[i][j] = prefix_sums[i - 1][j] + mat[i][j]
+        total = 0
+        for i1 in range(m):
+            for i2 in range(i1, m):
+                width = 0
+                for j in range(n):
+                    if i1 == 0:
+                        all_ones = prefix_sums[i2][j] == i2 + 1
+                    else:
+                        all_ones = prefix_sums[i2][j] - prefix_sums[i1 - 1][j] == (
+                            i2 - i1 + 1
+                        )
+                    if all_ones:
+                        width += 1
+                    else:
+                        width = 0
+                    total += width
+        return total
