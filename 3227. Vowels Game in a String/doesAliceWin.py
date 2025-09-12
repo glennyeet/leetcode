63d660e80b98e29ccf1d5e70032199487b832738
Glenn Ye <lcm7063@gmail.com>
2025-09-12 00:52:48 -0400

    Complete 3227

---
+++ 3227. Vowels Game in a String/doesAliceWin.py
@@ -0,0 +1,9 @@
+class Solution:
+    def doesAliceWin(self, s: str) -> bool:
+        # Game Theory: O(n) time, O(1) space, where n is the size of s
+
+        vowels = ["a", "e", "i", "o", "u"]
+        total_vowels = 0
+        for char in s:
+            total_vowels += char in vowels
+        return (total_vowels % 2 == 0 and total_vowels > 0) or total_vowels % 2 == 1
