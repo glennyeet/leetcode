373a8f2ba366768c6bf8f4bda3b51ea174f1282c
Glenn Ye <lcm7063@gmail.com>
2025-10-03 12:38:13 -0400

    Update 242

--- 242. Valid Anagram/isAnagram.py
+++ 242. Valid Anagram/isAnagram.py
@@ -3,4 +3,6 @@ from collections import Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # Hash Table: O(s + t) time, O(s + t) space
+
         return Counter(s) == Counter(t)
