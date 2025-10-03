cd3d2cab7187c29b6a5bb49632ad7fd6f4ddb21c
Glenn Ye <lcm7063@gmail.com>
2025-10-03 12:44:19 -0400

    Update 1

--- 1. Two Sum/twoSum2.py
+++ 1. Two Sum/twoSum2.py
@@ -10,6 +10,6 @@ class Solution:
         for i, num1 in enumerate(nums):
             num2 = target - num1
             if num2 in seen_nums:
-                return [i, seen_nums[num2]]
+                return [min(i, seen_nums[num2]), max(i, seen_nums[num2])]
             seen_nums[num1] = i
         return [-1, -1]
