f143e24b921f26954d21a904e9c45e8eb59d3e26
Glenn Ye <lcm7063@gmail.com>
2025-10-04 15:05:09 -0400

    Complete 15

---
+++ 15. 3Sum/threeSum.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Two Pointers + Hash Table: O(n * log(n)) time, O(n) space
+
+        n = len(nums)
+        sorted_nums = sorted(nums)
+        triplets = []
+        triplets_set = set()
+        for k in range(n):
+            i = 0
+            j = k - 1
+            while i < j:
+                cur_sum = sorted_nums[i] + sorted_nums[j] + sorted_nums[k]
+                triplet = (sorted_nums[i], sorted_nums[j], sorted_nums[k])
+                if triplet not in triplets_set and cur_sum == 0:
+                    triplets.append(list(triplet))
+                    triplets_set.add(triplet)
+                if cur_sum <= 0:
+                    i += 1
+                else:
+                    j -= 1
+        return triplets
