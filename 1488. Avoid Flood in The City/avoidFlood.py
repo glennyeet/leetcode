2be45c40e85e32a8e02e2273f51da321d122d5e8
Glenn Ye <lcm7063@gmail.com>
2025-10-07 16:04:26 -0400

    Update 1488

--- 1488. Avoid Flood in The City/avoidFlood.py
+++ 1488. Avoid Flood in The City/avoidFlood.py
@@ -13,16 +13,14 @@ class Solution:
         for i, lake in enumerate(rains):
             if lake == 0:
                 dry_operations.add(i)
-            elif lake not in full_lakes:
-                full_lakes[lake] = i
-            else:
+            elif lake in full_lakes:
                 j = dry_operations.bisect_left(full_lakes[lake])
-                if j >= len(dry_operations):
+                if j == len(dry_operations):
                     return []
                 dry_operation = dry_operations[j]
                 ans[dry_operation] = lake
                 dry_operations.remove(dry_operation)
-                full_lakes[lake] = i
+            full_lakes[lake] = i
         for dry_operation in dry_operations:
             ans[dry_operation] = 1
         return ans
