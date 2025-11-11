a86fd919109b737b08fa30f38dbba03505b8eeec
Glenn Ye <lcm7063@gmail.com>
2025-11-10 22:59:26 -0500

    Complete 474

---
+++ 474. Ones and Zeroes/findMaxForm.py
@@ -0,0 +1,34 @@
+from typing import List
+from functools import cache
+
+
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        # Top-down DP: O(s * m * n) time, O(s * m * n) space
+
+        s = len(strs)
+
+        @cache
+        def find_largest_subset_size(
+            index: int, total_zeroes: int, total_ones: int
+        ) -> int:
+            if index == s:
+                return 0
+            largest_subset_size = find_largest_subset_size(
+                index + 1, total_zeroes, total_ones
+            )
+            zeroes = strs[index].count("0")
+            ones = len(strs[index]) - zeroes
+            new_total_zeroes = total_zeroes + zeroes
+            new_total_ones = total_ones + ones
+            if new_total_zeroes <= m and new_total_ones <= n:
+                largest_subset_size = max(
+                    largest_subset_size,
+                    1
+                    + find_largest_subset_size(
+                        index + 1, new_total_zeroes, new_total_ones
+                    ),
+                )
+            return largest_subset_size
+
+        return find_largest_subset_size(0, 0, 0)
