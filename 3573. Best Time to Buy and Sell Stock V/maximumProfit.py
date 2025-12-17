f6f64b00a99ca599b5640ba68c70473aa8bf7b1b
Glenn Ye <lcm7063@gmail.com>
2025-12-17 03:50:02 -0500

    Complete 3573

---
+++ 3573. Best Time to Buy and Sell Stock V/maximumProfit.py
@@ -0,0 +1,40 @@
+from typing import List
+from functools import cache
+
+
+class Solution:
+    def maximumProfit(self, prices: List[int], k: int) -> int:
+        n = len(prices)
+
+        @cache
+        def can_buy(day: int, transactions: int) -> float | int:
+            if day == n:
+                return float("-inf")
+            return max(
+                can_buy(day + 1, transactions), -prices[day] + dp(day + 1, transactions)
+            )
+
+        @cache
+        def can_sell(day: int, transactions: int) -> float | int:
+            if day == n:
+                return float("-inf")
+            return max(
+                can_sell(day + 1, transactions), prices[day] + dp(day + 1, transactions)
+            )
+
+        @cache
+        def dp(day: int, transactions: int) -> int:
+            if day == n or transactions == k:
+                return 0
+            max_profit = max(
+                dp(day + 1, transactions),
+                prices[day] + can_buy(day + 1, transactions + 1),
+                -prices[day] + can_sell(day + 1, transactions + 1),
+            )
+            return max_profit
+
+        max_profit = dp(0, 0)
+        can_buy.cache_clear()
+        can_sell.cache_clear()
+        dp.cache_clear()
+        return max_profit
