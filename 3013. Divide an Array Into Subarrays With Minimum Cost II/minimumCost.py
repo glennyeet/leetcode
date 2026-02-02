c24e386c9317800f112c6163c140568f3b828073
Glenn Ye <lcm7063@gmail.com>
2026-02-01 20:48:20 -0500

    Update 3013

--- 3013. Divide an Array Into Subarrays With Minimum Cost II/minimumCost.py
+++ 3013. Divide an Array Into Subarrays With Minimum Cost II/minimumCost.py
@@ -4,28 +4,29 @@ from sortedcontainers import SortedList
 
 class Solution:
     def minimumCost(self, nums: List[int], k: int, dist: int) -> int:
-        # B-tree + Sliding Window: O(n * log(n)) time, O(n) space
+        # B-tree + Sliding Window: O(n * log(d)) time, O(d) space,
+        # where d is dist
 
         n = len(nums)
         min_sum = float("inf")
         cur_sum = 0
         start_window = SortedList()
         for right in range(1, n):
-            left = right - (dist + 1)
-            if left >= 1:
-                window_index = start_window.bisect_left((nums[left], left))
+            prev_left = right - (dist + 1)
+            if prev_left >= 1:
+                window_index = start_window.bisect_left((nums[prev_left], prev_left))
                 if 0 <= window_index < k - 1:
-                    cur_sum -= nums[left]
+                    cur_sum -= nums[prev_left]
                     if len(start_window) >= k:
                         cur_sum += start_window[k - 1][0]
-                start_window.remove((nums[left], left))
+                start_window.remove((nums[prev_left], prev_left))
             window_index = start_window.bisect_left((nums[right], right))
             if 0 <= window_index < k - 1:
                 cur_sum += nums[right]
                 if len(start_window) >= k - 1:
                     cur_sum -= start_window[k - 2][0]
             start_window.add((nums[right], right))
-            if left >= 0:
+            if prev_left >= 0:
                 min_sum = min(min_sum, cur_sum)
         min_sum += nums[0]
         return min_sum
