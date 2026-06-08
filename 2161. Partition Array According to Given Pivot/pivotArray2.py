4d32740fae1c005dd7ac4e2cdcd3f7d7b1b0fe4c
Glenn Ye <lcm7063@gmail.com>
2026-06-07 20:11:01 -0400

    Redo 2161

---
+++ 2161. Partition Array According to Given Pivot/pivotArray2.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        # Array: O(n) time, O(n) space, where n is the size of nums
+
+        less_than_pivot = []
+        equal_to_pivot = []
+        greater_than_pivot = []
+        for num in nums:
+            if num < pivot:
+                less_than_pivot.append(num)
+            elif num > pivot:
+                greater_than_pivot.append(num)
+            else:
+                equal_to_pivot.append(num)
+        return less_than_pivot + equal_to_pivot + greater_than_pivot
