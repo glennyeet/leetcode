02a22b56393d9be1bf79c885ca1f340482aab3ca
Glenn Ye <lcm7063@gmail.com>
2026-06-14 22:39:39 -0400

    Update 2130

--- 2130. Maximum Twin Sum of a Linked List/pairSum.py
+++ 2130. Maximum Twin Sum of a Linked List/pairSum.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 
-# Definition for singly-linked list.\
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
